a42c17d7251c9412a7095e9481f5739377413348
Garrett Dodd <83469501+gdodd1@users.noreply.github.com>
2022-02-10 14:20:05 -0600

    better locations

--- html/login-page.js
+++ html/login-page.js
@@ -8,10 +8,10 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "admin" && password === "prudential") {
-        location.replace("https://www.alphasiguna.com/brother-resources/Admin.html");
+        location.replace("https://forms.gle/ENyDup8vak9UYYER8");
     } 
     else if (username === "brother" && password === "bettertheman") {
-        location.replace("https://www.alphasiguna.com/brother-resources/Brothers.html");
+        location.replace("https://drive.google.com/drive/folders/1OFn0KlvGjCR5Lw-b4iqfOx3x7eLGN_BA?usp=sharing");
     }
     else {
         loginErrorMsg.style.opacity = 1;
